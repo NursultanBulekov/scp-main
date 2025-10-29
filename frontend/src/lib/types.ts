8cfa1ed0d116b017ca1357b3da22477f1fb7ce70
Nursultan Bulekov <bulekov0905@gmail.com>
2025-10-29 18:19:53 +0500

    share user role names

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,7 @@
+export enum RoleEnum {
+    Consumer = "consumer",
+    SupplierOwner = "supplier_owner",
+    SupplierManager = "supplier_manager",
+    SupplierSales = "supplier_sales",
+    PlatformAdmin = "platform_admin",
+}
