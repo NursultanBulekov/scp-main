fe678919c70e222eb4d0662a3674023492c3120e
Nursultan Bulekov <bulekov0905@gmail.com>
2025-11-16 21:20:06 +0500

    start complaints page

---
+++ frontend/src/app/dashboard/complaints/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import DashboardLayout from '@/components/DashboardLayout';
+
+export default function Page() {
+  return (
+    <DashboardLayout>
+      <h1 className="text-3xl font-bold mb-6">Complaints</h1>
+      <p>No complaints.</p>
+    </DashboardLayout>
+  );
+}
